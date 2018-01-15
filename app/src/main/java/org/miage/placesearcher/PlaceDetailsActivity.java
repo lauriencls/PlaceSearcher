8f9104ea8c9844552e5ab7e82753b91a7ce1cc68
lauriencls <laurie.divers@gmail.com>
2018-01-15 14:49:02 +0100

    Retour quand clic sur rue et ajout bouton recherche Google

--- app/src/main/java/org/miage/placesearcher/PlaceDetailsActivity.java
+++ app/src/main/java/org/miage/placesearcher/PlaceDetailsActivity.java
@@ -1,8 +1,13 @@
 package org.miage.placesearcher;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import butterknife.BindView;
@@ -13,6 +18,15 @@ public class PlaceDetailsActivity extends AppCompatActivity {
     @BindView(R.id.detail_street)
     TextView mDetailStreet;
 
+    @BindView(R.id.button_google_search)
+    Button mButtonGoogleSearch;
+
+    @BindView(R.id.button_share)
+    Button mButtonShare;
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,12 +34,40 @@ public class PlaceDetailsActivity extends AppCompatActivity {
         setContentView(R.layout.place_details_layout);
         ButterKnife.bind(this);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         String detailStreetInfo = getIntent().getStringExtra("detailInfoStreet");
         mDetailStreet.setText(detailStreetInfo);
 
+        /** Action quand on clique sur l'intitulé de la rue */
+        mDetailStreet.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                /** Première manière **/
+                PlaceDetailsActivity.this.finish();
+
+                /** Seconde manière
+                 * Intent mainActivityIntent = new Intent(PlaceDetailsActivity.this, MainActivity.class);
+                 * startActivity(mainActivityIntent);
+                 */
+            }
+        });
+
+
+        /** Action quand on clique sur "Chercher sur Google" */
+        mButtonGoogleSearch.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                Log.e("LOG", "Avant le Google");
+
+                /** Lancement recherche de la rue dans Google */
+                Uri url = Uri.parse("http://www.google.fr/search?q="+mDetailStreet.getText());
+                Intent launchBrowser = new Intent(Intent.ACTION_VIEW, url);
+                startActivity(launchBrowser);
+            }
+        });
 
     }
 
